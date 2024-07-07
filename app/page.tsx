8033d6e11fb18645a6aa7ea913e437e75e5aeb56
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-07 16:37:02 -0700

    adding content area component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@/components/ui/button"
 import SidebarHeader from "@/components/Sidebar/SidebarHeader"
 import SidebarNav from "@/components/Sidebar/SidebarNav"
 import SidebarFooter from "@/components/Sidebar/SidebarFooter"
 import Header from "@/components/Header"
+import ContentArea from "@/components/ContentArea"
 
 export default function Dashboard() {
   return (
@@ -20,24 +20,7 @@ export default function Dashboard() {
       </div>
       <div className="flex flex-col">
         <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-          </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
-        </main>
+        <ContentArea />
       </div>
     </div>
   )
