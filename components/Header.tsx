0178cdf4134ecb5e372dc7872b7c7bad0e5b247b
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-07 18:08:20 -0700

    adding UserButton component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,6 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { CircleUser, } from "lucide-react"
 import SidebarMobile from "./SidebarMobile/SidebarMobile"
 import SearchBar from "./SearchBar"
+import UserButton from "./UserButton"
 
 const Header = () => {
   return (
@@ -19,22 +9,7 @@ const Header = () => {
       <div className="w-full flex-1">
         <SearchBar />
       </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="secondary" size="icon" className="rounded-full">
-            <CircleUser className="h-5 w-5" />
-            <span className="sr-only">Toggle user menu</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <UserButton />
     </header>
   )
 }
