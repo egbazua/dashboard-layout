e07f8f2b3ff6be5c3998a6414e2afc6848c9bc69
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-07 18:12:34 -0700

    fixing searchbar component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,7 @@ const Header = () => {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <SidebarMobile />
-      <div className="w-full flex-1">
-        <SearchBar />
-      </div>
+      <SearchBar />
       <UserButton />
     </header>
   )
