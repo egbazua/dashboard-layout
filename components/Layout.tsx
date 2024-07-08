2795fef9078b922abe00fc138e8b2120204262c0
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-07 18:10:12 -0700

    fixing sidebar component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,9 +8,7 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <Sidebar />
-      </div>
+      <Sidebar />
       <div className="flex flex-col">
         <Header />
         {children}
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -4,13 +4,15 @@ import SidebarNav from "./SidebarNav"
 
 const Sidebar = () => {
   return (
-    <div className="flex h-full max-h-screen flex-col gap-2">
-      <SidebarHeader />
-      <div className="flex-1">
-        <SidebarNav />
-      </div>
-      <div className="mt-auto p-4">
-        <SidebarFooter />
+    <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <SidebarHeader />
+        <div className="flex-1">
+          <SidebarNav />
+        </div>
+        <div className="mt-auto p-4">
+          <SidebarFooter />
+        </div>
       </div>
     </div>
   )
