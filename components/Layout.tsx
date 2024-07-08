993bea7287b6dbb9f5da06517590a981e309642b
Enrique Bazúa <enriquegbazua@gmail.com>
2024-07-07 17:55:20 -0700

    implementing layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import Header from "./Header"
+import Sidebar from "./Sidebar"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <Sidebar />
+      </div>
+      <div className="flex flex-col">
+        <Header />
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
